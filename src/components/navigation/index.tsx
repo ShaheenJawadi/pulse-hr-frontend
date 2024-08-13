3022bbdec6ed0e05f3e43242296d1de45d3124df
shaheenj <jawadishaheen@gmail.com>
2024-08-14 00:57:19 +0100

    refactor: Add logo to Navbar component structure

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 import { styled, useTheme, Theme, CSSObject } from "@mui/material/styles";
 import MuiDrawer from "@mui/material/Drawer";
- 
+import Image from "next/image";
+
 import Divider from "@mui/material/Divider";
-import IconButton from "@mui/material/IconButton"; 
-import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
-import FiberSmartRecordIcon from '@mui/icons-material/FiberSmartRecord';
+import IconButton from "@mui/material/IconButton";
+import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
+import FiberSmartRecordIcon from "@mui/icons-material/FiberSmartRecord";
 import NavigationMenu from "./menu";
+import { Box, Stack } from "@mui/material";
 
 const drawerWidth = 260;
 
@@ -57,38 +59,56 @@ const Drawer = styled(MuiDrawer, {
 }));
 
 export default function Navigation() {
+  const [open, setOpen] = React.useState<boolean>(true);
+  const [isHovered, setIsHovered] = React.useState(false);
 
-    const [open , setOpen]= React.useState<boolean>(true);
-    const [isHovered, setIsHovered] = React.useState(false);
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
 
-    const handleMouseEnter = () => {
-      setIsHovered(true);  
-    };
-  
-    const handleMouseLeave = () => {
-      setIsHovered(false);  
-    };
-    const handleDrawerOpen = () => {
-        setOpen(!open);
-      };
- 
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+  const handleDrawerOpen = () => {
+    setOpen(!open);
+  };
+
   return (
-    <Drawer variant="permanent" open={open || isHovered}      onMouseEnter={handleMouseEnter}
-    onMouseLeave={handleMouseLeave}>
+    <Drawer
+      variant="permanent"
+      open={open || isHovered}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       <DrawerHeader>
-        <IconButton  onClick={()=>handleDrawerOpen()}>
-
-            {
-                open ? <FiberSmartRecordIcon sx={{fontSize:20}} />:isHovered ?  <FiberManualRecordIcon  sx={{fontSize:20}} /> :null
-            }
-          
-          
-
-        </IconButton>
+        <Stack direction={"row"} flex={1} justifyContent={"space-between"}>
+          <Box
+            sx={{
+              flex: 1,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+            flex={1}
+          >
+            <Image
+              src="/logo/logo.svg"
+              width={180}
+              height={60}
+              alt="Picture of the author"
+            />
+          </Box>
+          <IconButton onClick={() => handleDrawerOpen()}>
+            {open ? (
+              <FiberSmartRecordIcon sx={{ fontSize: 20 }} />
+            ) : isHovered ? (
+              <FiberManualRecordIcon sx={{ fontSize: 20 }} />
+            ) : null}
+          </IconButton>{" "}
+        </Stack>
       </DrawerHeader>
       <Divider />
-      <NavigationMenu isOpen={open  || isHovered}/>
-      
+      <NavigationMenu isOpen={open || isHovered} />
     </Drawer>
   );
 }
