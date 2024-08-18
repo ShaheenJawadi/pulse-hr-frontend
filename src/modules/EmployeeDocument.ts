ce0e9cceb30b8719f980919f050dab857693823a
shaheenj <jawadishaheen@gmail.com>
2024-08-18 10:26:32 +0100

    Add EmployeeDocument interface

---
+++ src/modules/EmployeeDocument.ts
@@ -0,0 +1,9 @@
+export interface EmployeeDocument {
+    id?: number;
+    employee_id: number;
+    document_type: string;
+    document_path?: string;
+    uploaded_at?: string;
+    created_at?: string;
+    updated_at?: string;
+}
