896153d354ba7338fcf00a6ff8d14528333101e2
shaheenj <jawadishaheen@gmail.com>
2024-08-20 07:49:55 +0100

    refactor: Update login reset password components

--- src/app/(auth)/resetPws/page.tsx
+++ src/app/(auth)/resetPws/page.tsx
@@ -1,13 +1,68 @@
-import { Box } from "@mui/material";
-
+"use client";
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  InputAdornment, 
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import LockIcon from "@mui/icons-material/Lock";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const ResetPws = () => {
+  const router = useRouter();
 
+  const openResetTest = () => {
 
+    router.push("/resetPws");
+
+  }
+  
   return (
-    <Box >
-        reset pws
-    </Box>
+    <Stack spacing={1} height={"100%"}>
+      <Typography variant="h5">Réinitialiser le mot de passe</Typography>
+      <Stack spacing={5} justifyContent={"center"} height={"100%"}>
+      <TextField
+          type="password"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <LockIcon />
+              </InputAdornment>
+            ),
+          }}
+          sx={{ width: "100%" }}
+          label="Saisir un nouveau mot de passee"
+          variant="outlined"
+        />
+          <TextField
+          type="password"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <LockIcon />
+              </InputAdornment>
+            ),
+          }}
+          sx={{ width: "100%" }}
+          label="Ressaisissez votre nouveau mot de passe"
+          variant="outlined"
+        />
+
+        <Button onClick={()=>openResetTest()} variant="contained" size="large">
+          Réinitialiser 
+        </Button>
+
+        <Link href={"/login"}>
+          <Typography variant="body2">Se connecter !</Typography>
+        </Link>
+      </Stack>
+    </Stack>
   );
 };
 
