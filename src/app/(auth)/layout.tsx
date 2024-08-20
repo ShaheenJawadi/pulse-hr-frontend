ec54730d5d3f85edf8eed7307a8fbe2e24f04113
shaheenj <jawadishaheen@gmail.com>
2024-08-20 07:37:57 +0100

    refactor: Update auth layout and login page components

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,7 +2,7 @@
 import { Box, Card, Container, Dialog, Stack, Typography } from "@mui/material";
 import { ChildrenType } from "@/types/themeTypes";
 import Image from "next/image";
-import WavingHandIcon from '@mui/icons-material/WavingHand';
+import WavingHandIcon from "@mui/icons-material/WavingHand";
 const Layout = async ({ children }: ChildrenType) => {
   return (
     <Box
@@ -14,12 +14,15 @@ const Layout = async ({ children }: ChildrenType) => {
       <Card variant="elevation" sx={{ minWidth: 900, minHeight: 600 }}>
         <Stack direction={"row"}>
           <Image src="/auth/img.jpg" width={420} height={600} alt="logo" />
-          <Stack flex={1} spacing={6} margin={10} alignItems={"center"}>
-            <Image  src="/logo/logo.svg" width={240} height={80} alt="logo" />
-            <Stack width={"100%"} direction={"row"} spacing={4} > <Typography variant="h5">Rebonjour </Typography> <WavingHandIcon color="primary"/></Stack>
-           
-
-            <Box>{children}</Box>
+          <Stack flex={1} spacing={4} margin={10} alignItems={"center"}>
+            <Image src="/logo/logo.svg" width={240} height={80} alt="logo" />
+            <Stack direction={"row"} spacing={4}>
+              <Typography variant="body1">Rebonjour </Typography>
+              <WavingHandIcon color="primary" />
+            </Stack>
+            <Stack flex={1} width={"100%"} spacing={2}>
+              <>{children}</>
+            </Stack>
           </Stack>
         </Stack>
       </Card>
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,13 +1,58 @@
-import { Box } from "@mui/material";
 
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  InputAdornment, 
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import LockIcon from "@mui/icons-material/Lock";
+import Link from "next/link";
 
 const Login = () => {
-
-
   return (
-    <Box >
-        login
-    </Box>
+    <Stack spacing={1} height={"100%"}>
+      <Typography variant="h5">Se connecter</Typography>
+      <Stack spacing={5} justifyContent={"center"} height={"100%"}>
+        <TextField
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <AlternateEmailIcon />
+              </InputAdornment>
+            ),
+          }}
+          sx={{ width: "100%" }}
+          label="Email"
+          variant="outlined"
+        />
+        <TextField
+          type="password"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <LockIcon />
+              </InputAdornment>
+            ),
+          }}
+          sx={{ width: "100%" }}
+          label="Mot de passe"
+          variant="outlined"
+        />
+
+        <Button variant="contained" size="large">
+          Se connecter
+        </Button>
+
+        <Link href={"/forgotPws"}>
+          <Typography variant="body2">Mot de passe oublié ?</Typography>
+        </Link>
+      </Stack>
+    </Stack>
   );
 };
 
