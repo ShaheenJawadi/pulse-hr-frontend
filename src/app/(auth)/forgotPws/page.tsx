64e897c6bcadf8cededcfc9171d3dd7fb13cedee
shaheenj <jawadishaheen@gmail.com>
2024-08-20 07:47:05 +0100

    refactor: Update login forgot password components

--- src/app/(auth)/forgotPws/page.tsx
+++ src/app/(auth)/forgotPws/page.tsx
@@ -1,13 +1,55 @@
-import { Box } from "@mui/material";
-
+"use client";
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  InputAdornment, 
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import LockIcon from "@mui/icons-material/Lock";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const ForgotPws = () => {
+  const router = useRouter();
 
+  const openResetTest = () => {
 
+    router.push("/resetPws");
+
+  }
+  
   return (
-    <Box >
-        f pws
-    </Box>
+    <Stack spacing={1} height={"100%"}>
+      <Typography variant="h5">Mot de passe oublié ?</Typography>
+      <Stack spacing={5} justifyContent={"center"} height={"100%"}>
+        <TextField
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <AlternateEmailIcon />
+              </InputAdornment>
+            ),
+          }}
+          sx={{ width: "100%" }}
+          label="Veuillez entrer votre email"
+          variant="outlined"
+        />
+         
+
+        <Button onClick={()=>openResetTest()} variant="contained" size="large">
+          Réinitialiser 
+        </Button>
+
+        <Link href={"/login"}>
+          <Typography variant="body2">Se connecter !</Typography>
+        </Link>
+      </Stack>
+    </Stack>
   );
 };
 
