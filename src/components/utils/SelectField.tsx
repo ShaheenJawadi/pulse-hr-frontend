b573a2916c58784e2aa3cf5fe32731f8ba718bb6
shaheenj <jawadishaheen@gmail.com>
2024-08-25 16:03:28 +0100

    refactor: Add SelectField component for form select input handling

---
+++ src/components/utils/SelectField.tsx
@@ -0,0 +1,97 @@
+import { SelectDataTypes } from "@/types/structureTypes";
+import {
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  FormHelperText,
+  MenuItem,
+  Select,
+  MenuItemProps,
+  styled,
+  alpha,
+  Button,
+  Typography,
+  Stack,
+  SelectChangeEvent,
+  TextField,
+  SelectProps,
+} from "@mui/material";
+
+import {
+  ChangeEventHandler,
+  ReactElement,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { UseFormRegisterReturn } from "react-hook-form";
+
+type Props = SelectProps & {
+  label: string;
+  formRegistartion?: UseFormRegisterReturn;
+  isError?: boolean;
+  errorMessage?: string;
+  selectData: SelectDataTypes[];
+  dataValue?: any;
+  getNewValue?: (value: any) => void;
+  allowNull?: string;
+};
+
+export const SelectField = (props: Props) => {
+  const {
+    label,
+    formRegistartion,
+    isError,
+    errorMessage,
+    selectData,
+    dataValue,
+    allowNull,
+    getNewValue,
+  } = props;
+
+  const [selectVal, setSelectVal] = useState(dataValue);
+
+  const handleChange = (event: any) => {
+    setSelectVal(event?.target?.value as string);
+
+    if (getNewValue) {
+      getNewValue(event?.target?.value);
+    }
+  };
+
+  useEffect(() => {
+
+    setSelectVal(dataValue)
+}, [dataValue])
+
+  return (
+    <FormControl fullWidth error={isError}>
+      <InputLabel>{label}</InputLabel>
+      <Select
+        error={isError}
+        {...props}
+        inputProps={formRegistartion}
+        onChange={handleChange}
+        value={selectVal}
+      >
+        {allowNull && (
+          <MenuItem value={undefined}>
+            <Stack direction={"row"} spacing={2}>
+              <Typography>{allowNull} </Typography>
+            </Stack>
+          </MenuItem>
+        )}
+
+        {selectData?.map((item, index) => {
+          return (
+            <MenuItem key={index} value={item.value}>
+              {item.labelText}
+            </MenuItem>
+          );
+        })}
+      </Select>
+
+      <FormHelperText>{errorMessage}</FormHelperText>
+    </FormControl>
+  );
+};
