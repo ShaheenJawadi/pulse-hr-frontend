8981862b056752162a005d387cfbd260a4c947a4
shaheenj <jawadishaheen@gmail.com>
2024-08-27 14:14:16 +0100

    refactor: Update applications list page with collapsible candidate information

--- src/app/(dashboard)/recruitment/applications/[id]/page.tsx
+++ src/app/(dashboard)/recruitment/applications/[id]/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import CardListing from "@/components/listingPages/cardsListing";
 import PagerHeader from "@/components/listingPages/pageHeader";
 import {
@@ -6,15 +8,21 @@ import {
   ButtonGroup,
   Card,
   CardContent,
+  CardHeader,
   Chip,
   Divider,
   Grid,
+  IconButton,
   Stack,
   Typography,
 } from "@mui/material";
 import LocationOnTwoToneIcon from "@mui/icons-material/LocationOnTwoTone";
 import SingleJobGrid from "@/components/pages/recruitement/SingleJobGrid";
 import { SelectField } from "@/components/utils/SelectField";
+import KeyboardDoubleArrowUpIcon from "@mui/icons-material/KeyboardDoubleArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { useState } from "react";
+import { CustomInfoData } from "@/components/utils/InfoDataCustom";
 
 const ApplicationsListPage = () => {
   return (
@@ -28,7 +36,7 @@ const ApplicationsListPage = () => {
           <Grid item sm={7}>
             <Stack spacing={2}>
               <Stack
-                direction={"row"} 
+                direction={"row"}
                 justifyContent={"space-between"}
                 alignItems={"center"}
                 spacing={2}
@@ -44,7 +52,7 @@ const ApplicationsListPage = () => {
                   <Button color="secondary">Toutes</Button>
                 </ButtonGroup>
               </Stack>
-              {[1, 2, 3 ].map((item) => {
+              {[1, 2, 3].map((item) => {
                 return <SingleApplication />;
               })}
             </Stack>
@@ -57,7 +65,11 @@ const ApplicationsListPage = () => {
 
 const JobDescripton = () => {
   return (
-    <Stack sx={{position:"sticky" , top:30}} alignItems={"center"} justifyContent={"center"}>
+    <Stack
+      sx={{ position: "sticky", top: 30 }}
+      alignItems={"center"}
+      justifyContent={"center"}
+    >
       <Box sx={{ maxWidth: 800 }}>
         <Card>
           <CardContent>
@@ -71,17 +83,89 @@ const JobDescripton = () => {
 };
 
 const SingleApplication = () => {
+  const [openInfo, setOpenInfo] = useState(false);
+
   return (
     <Stack>
-      <CardListing
-        title={"Nom & prenom"}
-        notReverse={true}
-        avatar="/utils/goat.jpg" 
-        customAction={
-          <Chip color="warning" variant={"outlined"} label={"En attente"} />
-        }
-      ></CardListing>
+      <Card>
+        <Box>
+          <Stack
+            direction={"row"}
+            justifyContent={"space-between"}
+            alignItems={"center"}
+            padding={2}
+            onClick={() => setOpenInfo(!openInfo)}
+          >
+            <Stack spacing={1}>
+              <Typography variant="body1" color={"secondary"}>
+                Nom & prenom
+              </Typography>
+              <Typography color={"secondaryy"}>
+                jawadishaheen@gmail.com
+              </Typography>
+              <Box>
+                {/*  <Chip
+                  color="warning"
+                  size="small"
+                  variant={"outlined"}
+                  label={"En attente"}
+                /> */}
+              </Box>
+            </Stack>
+            <Box>
+              <IconButton color="secondary">
+                {openInfo ? (
+                  <KeyboardDoubleArrowUpIcon />
+                ) : (
+                  <KeyboardArrowDownIcon />
+                )}
+              </IconButton>
+            </Box>
+          </Stack>
+          <Divider />
+        </Box>
+        {openInfo && <CandidatInfo />}
+      </Card>
     </Stack>
+  );
+};
+
+const CandidatInfo = () => {
+  return (
+    <CardContent >
+      <Stack spacing={1}>
+        <CustomInfoData title={"Nom :"} info={"Jawadi"} />
+        <CustomInfoData title={"Prenom :"} info={"shaheen"} />
+        <CustomInfoData title={"Email :"} info={"jawadishaheen@gmail.com"} />
+        <CustomInfoData title={"Télephone :"} info={"52723344"} />
+        <CustomInfoData title={"Date de naissance  :"} info={"12/04/1998"} />
+        <CustomInfoData title={"Poste actuel  :"} info={"Full Stack dev"} />
+        <CustomInfoData title={"LinkedIn :"} info={"linkedin.com/"} />
+        <CustomInfoData title={"Github :"} info={"https://github.com/ShaheenJawadi"} />
+        <CustomInfoData title={"Motivation :"} info={"qsjkhd jsqdtgq sgduy za g uagyz gyuyzagdhgza jd fzaudfazfdhzagfduyzfa ud uzyfduyazhdigajkzgdjzahgduyazgdjaghzduyg azudyfajhzgfdsqghfdhgfqshdqydtazdapodjmlasdjoiqji"} />
+
+
+
+        <Button>Cv</Button>
+        <Button>lettre de motivation</Button>
+
+
+
+<Box>
+    <Stack marginTop={4} direction={"row"} alignContent={"center"} alignItems={"center"} justifyContent={"center"} spacing={4}>
+        <Button variant="contained"  color="error">rejeter</Button>
+        <Button  variant="contained" color="success">mis en présélection</Button>
+
+    </Stack>
+</Box>
+
+
+
+
+
+
+      </Stack>
+    </CardContent>
   );
 };
 
