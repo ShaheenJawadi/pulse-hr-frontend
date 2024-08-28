49f985d101fb1bd6d5364278f5601c8d7d7af793
shaheenj <jawadishaheen@gmail.com>
2024-08-28 09:42:46 +0100

    feat: Initiate Kanban main structure
    - Add horizontal scrolling to KanbanPage

--- src/app/(dashboard)/tools/kanban/page.tsx
+++ src/app/(dashboard)/tools/kanban/page.tsx
@@ -1,11 +1,41 @@
+"use client";
+import { Box, Stack } from "@mui/material";
+import PerfectScrollbar from "react-perfect-scrollbar";
 const KanbanPage = () => {
+  return (
+    <Box>
+    <PerfectScrollbar dir="horizontal">
+ 
+      <Stack direction={"row"} spacing={4}>
+        <Box minWidth={250}>Col</Box>
 
+        <Box minWidth={250}>Col</Box>
 
-    return (
-        <div>
-           tasks
-        </div>
-    )
-}
+        <Box minWidth={250}>Col</Box>
+
+        <Box minWidth={250}>Col</Box>
+
+        <Box minWidth={250}>Col</Box>
+
+        <Box minWidth={250}>Col</Box>
+
+        <Box minWidth={250}>Col</Box>
+
+        <Box minWidth={250}>Col</Box>
+        
+        <Box minWidth={250}>Col</Box>
+
+        <Box minWidth={250}>Col</Box>
+
+        <Box minWidth={250}>Col</Box>
+
+        <Box minWidth={250}>Col</Box>
+
+        <Box minWidth={250}>Col</Box>
+      </Stack>
+    </PerfectScrollbar>
+    </Box>  
+  );
+};
 
 export default KanbanPage;
