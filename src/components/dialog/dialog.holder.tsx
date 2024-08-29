3cc72da4c0f0134f5823f171fa16a8059ddac166
shaheenj <jawadishaheen@gmail.com>
2024-08-29 15:22:41 +0100

    refactor: Update dialog holder to dynamically import clock in and clock out components

--- src/components/dialog/dialog.holder.tsx
+++ src/components/dialog/dialog.holder.tsx
@@ -1,12 +1,23 @@
-"use client";
-import ClockInDialog from "../pages/clockInOut/clockIn";
-import ClockOutDialog from "../pages/clockInOut/clockOut";
+"use client"; 
 import CommonDialog from "./dialog";
 import { useDialogAction, useDialogState } from "./dialog.context";
 import dynamic from "next/dynamic";
 
+
+
+
 const ProcessLeave = dynamic(
   () => import("@/components/pages/leave/processLeave")
+);
+
+
+const ClockInDialog = dynamic(
+  () => import("@/components/pages/clockInOut/clockIn")
+);
+
+
+const ClockOutDialog = dynamic(
+  () => import("@/components/pages/clockInOut/clockOut")
 );
 
 type DialogHType = {
