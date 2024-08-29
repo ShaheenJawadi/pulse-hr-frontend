ea8df124b5049087da5e22c1b24b7fd4db3258dd
shaheenj <jawadishaheen@gmail.com>
2024-08-29 12:09:51 +0100

    refactor: Update card component styles for lightone variant

--- src/theme/overrides/card.ts
+++ src/theme/overrides/card.ts
@@ -8,6 +8,65 @@ const MUICard = (): Theme['components'] => {
             },
             variants: [
                 {
+                    props: { variant: 'lightone', color: 'primary' },
+                    style: {
+                        backgroundColor: 'var(--mui-palette-primary-lightOpacity)',
+                        color: 'var(--mui-palette-primary-main)',
+                        fontWeight: 500,
+
+                    }
+                },
+                {
+                    props: { variant: 'lightone', color: 'secondary' },
+                    style: {
+                        backgroundColor: 'var(--mui-palette-secondary-lightOpacity)',
+                        color: 'var(--mui-palette-secondary-main)',
+                        fontWeight: 500,
+
+
+                    }
+                },
+                {
+                    props: { variant: 'lightone', color: 'error' },
+                    style: {
+                        backgroundColor: 'var(--mui-palette-error-lightOpacity)',
+                        color: 'var(--mui-palette-error-main)',
+                        fontWeight: 500,
+
+                    }
+                },
+                {
+                    props: { variant: 'lightone', color: 'warning' },
+                    style: {
+                        backgroundColor: 'var(--mui-palette-warning-lightOpacity)',
+                        color: 'var(--mui-palette-warning-main)',
+                        fontWeight: 500,
+
+
+                    }
+                },
+                {
+                    props: { variant: 'lightone', color: 'info' },
+                    style: {
+                        backgroundColor: 'var(--mui-palette-info-lightOpacity)',
+                        color: 'var(--mui-palette-info-main)',
+                        fontWeight: 500,
+
+
+
+                    }
+                },
+                {
+                    props: { variant: 'lightone', color: 'success' },
+                    style: {
+                        backgroundColor: 'var(--mui-palette-success-lightOpacity)',
+                        color: 'var(--mui-palette-success-main)',
+                        fontWeight: 500,
+
+
+                    }
+                },
+                {
                     props: { is: "bg-primary" },
                     style: {
                         backgroundColor: "var(--mui-palette-primary-main)",
