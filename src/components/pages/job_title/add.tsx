35a854e840d0fc097d020ca0c9e31ff162c4caf9
shaheenj <jawadishaheen@gmail.com>
2024-08-31 20:42:51 +0100

    refactor: Update AddPoste component with form validation and submission logic

--- src/components/pages/job_title/add.tsx
+++ src/components/pages/job_title/add.tsx
@@ -1,15 +1,50 @@
+import { useDrawerAction } from "@/components/drawer/drawer.context";
 import { InputField } from "@/components/utils/InputField";
-import { Button, Stack, TextField } from "@mui/material";
+import useFormWithMutation from "@/hooks/postFormHook";
+import {
+  WorkPositionDefaultValues,
+  createWorkPositionSchema,
+} from "@/modules/WorkPosition";
+
+import { WpService } from "@/services/workPosition";
+import { Box, Button, Stack, TextField } from "@mui/material";
 
 const AddPoste = () => {
-  return (
-    <Stack spacing={5} justifyContent={"center"} height={"100%"}>
-      <InputField label="Designation" type={"text"} />
+  const mutation = WpService.useCreateMutation();
+  const { closeDrawer } = useDrawerAction();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    onSubmit,
+  } = useFormWithMutation(
+    WorkPositionDefaultValues,
+    createWorkPositionSchema,
+    mutation.mutateAsync,
+    () => handleSuccess(),
+    "Poste créé avec succès"
+  );
 
-      <Button variant="contained" size="large">
-        Créer
-      </Button>
-    </Stack>
+  const handleSuccess = () => {
+    closeDrawer();
+  };
+
+  return (
+    <Box component={"form"} onSubmit={handleSubmit(onSubmit)}>
+      <Stack spacing={5} justifyContent={"center"} height={"100%"}>
+        <InputField
+          formRegistartion={register("designation")}
+          isError={errors?.designation ? true : false}
+          errorMessage={errors?.designation?.message}
+          label="Designation"
+          type={"text"}
+        />
+
+        <Button type="submit" variant="contained" size="large">
+          Créer
+        </Button>
+      </Stack>
+    </Box>
   );
 };
 
