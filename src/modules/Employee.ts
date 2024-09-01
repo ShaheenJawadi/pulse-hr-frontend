9a6d0d3274258988d1f94f9fccc835ce91cc2e3f
shaheenj <jawadishaheen@gmail.com>
2024-09-01 10:53:35 +0100

    refactor: Update Employee interface to EmployeeType

--- src/modules/Employee.ts
+++ src/modules/Employee.ts
@@ -1,9 +1,19 @@
-export interface Employee {
-    id?: number;
+
+export type EmployeeType = {
+    name: string;
+    last_name: string;
+    email: string;
+    phone: string;
+    birthday: string;
+    sexe: string;
+    avatar?: string;
     hire_date: string;
-    contract_type: string;
-    department_id: number | null;
-    position: string;
-    created_at?: string;
-    updated_at?: string;
-}
+    end_contract?: string;
+    contract_type_id: number;
+    department_id: number;
+    shift_id: number;
+    supervisor_id?: number;
+    position_id: number;
+    additional_infos?: string;
+    user_id: number;
+};
