eca68afdffac00ea8e61faa3b2be5740f5773dca
shaheenj <jawadishaheen@gmail.com>
2024-09-01 13:11:27 +0100

    refactor: Update EmployeeSchema in Employee.ts for additional infos

--- src/modules/Employee.ts
+++ src/modules/Employee.ts
@@ -114,11 +114,11 @@ export const EmployeeSchema = yup.object().shape({
  const EmployeeSchemaStep4 = yup.object().shape({
 
     additional_infos: yup.object().shape({
-        contactName: yup.string().required("Entrez un nom pour le contact d'urgence"),
-        contactRelation: yup.string().required("Entrez une relation pour le contact d'urgence"),
-        contactPhone: yup.string().required("Entrez un numéro de téléphone pour le contact d'urgence"),
-        maritalStatus: yup.string().required("Sélectionnez un état civil"),
-        bloodGroup: yup.string().required("Sélectionnez un groupe sanguin"),
+        contactName: yup.string(),
+        contactRelation: yup.string(),
+        contactPhone: yup.string(),
+        maritalStatus: yup.string(),
+        bloodGroup: yup.string(),
       })
  
 });
