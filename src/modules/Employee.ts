d70bcb1d4ab5b0b60002efffaf84d3ffb7e38ed3
shaheenj <jawadishaheen@gmail.com>
2024-09-01 11:23:27 +0100

    refactor: Update EmployeeSchema in Employee.ts for step-wise form validation and submission

--- src/modules/Employee.ts
+++ src/modules/Employee.ts
@@ -29,20 +29,20 @@ export const EmployeeDefaultValues: EmployeeType = {
     phone: "",
     birthday: "",
     sexe: "",
-    avatar: null, 
+    avatar: null,
     hire_date: "",
-    end_contract: null, 
+    end_contract: null,
     contract_type_id: 0,
     department_id: 0,
     shift_id: 0,
-    supervisor_id: null,  
+    supervisor_id: null,
     position_id: 0,
-    additional_infos: null,  
+    additional_infos: null,
     user_id: 0,
 };
 
 
- 
+/* 
 export const EmployeeSchema = yup.object().shape({
     name: yup.string().required("Entrez un nom"),
     last_name: yup.string().required("Entrez un prénom"),
@@ -58,5 +58,51 @@ export const EmployeeSchema = yup.object().shape({
     shift_id: yup.number().required("Sélectionnez un horaire"),
     supervisor_id: yup.number().nullable().defined(),
     position_id: yup.number().required("Sélectionnez un poste"),
-    additional_infos: yup.string().nullable().defined(),  
+    additional_infos: yup.string().nullable().defined(),
+}); */
+
+
+
+ const EmployeeSchemaStep1 = yup.object().shape({
+    name: yup.string().required("Entrez un nom"),
+    last_name: yup.string().required("Entrez un prénom"),
+    email: yup.string().email("Entrez un email valide").required("Entrez un email"),
+    phone: yup.string().required("Entrez un numéro de téléphone"),
+    birthday: yup.string().required("Entrez une date de naissance"),
+    sexe: yup.string().oneOf(["h", "f"]).required("Sélectionnez un sexe"),
+    avatar: yup.string().nullable().defined(),
 });
+
+
+ const EmployeeSchemaStep2 = yup.object().shape({
+    department_id: yup.number().required("Sélectionnez un département"),
+    position_id: yup.number().required("Sélectionnez un poste"),
+    supervisor_id: yup.number().nullable().defined(),
+    shift_id: yup.number().required("Sélectionnez un horaire"),
+
+    hire_date: yup.string().required("Entrez une date d'embauche"),
+    contract_type_id: yup.number().required("Sélectionnez un type de contrat"),
+    end_contract: yup.string().nullable().defined(),
+
+
+
+
+});
+
+
+ const EmployeeSchemaStep3 = yup.object().shape({
+
+
+
+});
+
+
+ const EmployeeSchemaStep4 = yup.object().shape({
+
+
+
+    additional_infos: yup.string().nullable().defined(),
+});
+
+
+export const EmployeeSchema  = [EmployeeSchemaStep1, EmployeeSchemaStep2, EmployeeSchemaStep3 ,EmployeeSchemaStep4];
