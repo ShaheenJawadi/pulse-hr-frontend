3a2e6590db96d7ff9de5af33a2c44b7885f0b823
shaheenj <jawadishaheen@gmail.com>
2024-09-02 19:10:25 +0100

    refactor: Styling Clock Componant

--- src/components/pages/dashboard/topSection.tsx
+++ src/components/pages/dashboard/topSection.tsx
@@ -2,11 +2,6 @@ import AppReactDatepicker from "@/components/utils/datePicker";
 import { Box, Card, CardContent, Grid, Stack, Typography } from "@mui/material";
 
 const DashboardTopSection = () => {
-
-
-    const randomBg = Math.floor(Math.random() * 11) + 1;
-
-    const imgBg=randomBg;
   return (
     <>
       <Grid container spacing={3}>
@@ -24,22 +19,7 @@ const DashboardTopSection = () => {
         </Grid>
         <Grid item xs={4.5}>
           <Stack spacing={3}>
-            <Box>
-              <Card
-                sx={{
-                  backgroundImage: `url(/dashboard/clock_bg/${imgBg}.jpg)`, 
-                  backgroundSize: "cover",
-                  borderRadius: 2,
-                  backgroundPosition: "center",
-                  height: 150, 
-                
-                }}
-              >
-                <CardContent >
-                  <Typography>12;30</Typography>
-                </CardContent>
-              </Card>
-            </Box>
+            <ClockComponant />
             <Box>
               <AppReactDatepicker inline />
             </Box>
@@ -50,4 +30,42 @@ const DashboardTopSection = () => {
   );
 };
 
+const ClockComponant = () => {
+  const randomBg = Math.floor(Math.random() * 11) + 1;
+
+  const imgBg = randomBg;
+
+  return (
+    <Box>
+      <Card
+        sx={{
+          backgroundImage: `url(/dashboard/clock_bg/${imgBg}.jpg)`,
+          backgroundSize: "cover",
+          borderRadius: 2,
+          backgroundPosition: "center",
+          height: 150,
+        }}
+      >
+        <CardContent sx={{ height: "100%" }}>
+          <Stack
+            sx={{ height: "100%" }}
+            alignItems={"center"}
+            justifyContent={"center"}
+          >
+            <Typography
+              variant="h4"
+              sx={{
+                fontSize: "6.5rem",
+                color: "#fff",
+                textShadow: "1px 4px 10px #070421a3",
+              }} 
+            >
+              12:30
+            </Typography>
+          </Stack>
+        </CardContent>
+      </Card>
+    </Box>
+  );
+};
 export default DashboardTopSection;
