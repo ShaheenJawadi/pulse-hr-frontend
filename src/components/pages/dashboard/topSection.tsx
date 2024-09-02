9d71c19474a2c9a4dd3fbd6b89e1fa0b140b4eac
shaheenj <jawadishaheen@gmail.com>
2024-09-02 20:23:28 +0100

    refactor: Styling dashboard top section

--- src/components/pages/dashboard/topSection.tsx
+++ src/components/pages/dashboard/topSection.tsx
@@ -1,21 +1,44 @@
 import AppReactDatepicker from "@/components/utils/datePicker";
 import { Box, Card, CardContent, Grid, Stack, Typography } from "@mui/material";
 import { useState, useEffect } from "react";
+import Image from "next/image";
 
 const DashboardTopSection = () => {
+
+    const [imgBg, setImgBg] = useState<number>(1);
+
+    const changeImgBg = () => {
+
+        if (imgBg<14) {
+            setImgBg(imgBg+1)
+            
+        }
+        else {
+            setImgBg(imgBg-1)
+        }
+
+    }
+
   return (
     <>
       <Grid container spacing={3}>
         <Grid item xs={7.5}>
-          <Card is={"bg-secondary"}>
-            <CardContent>
-              <Typography variant="h5">sdg jsqhdgjsqgdjhsq dqsdsqd</Typography>
-              <Typography variant="body2">
-                sqdqsdqsdqsdsqd
-                <br />
-                {'"qsdqsdqsdqsd sqdqsdqsd"'}
-              </Typography>
-            </CardContent>
+          <Card sx={{padding:0}} variant="lightone" color="primary">
+            <Stack direction={"row"} justifyContent={"space-between"}>
+              <CardContent>
+                <Typography variant="h5">
+                  sdg jsqhdgjsqgdjhsq dqsdsqd
+                </Typography>
+                <Typography variant="body2">
+                  sqdqsdqsdqsdsqd
+                  <br />
+                  {'"qsdqsdqsdqsd sqdqsdqsd"'}
+                </Typography>
+              </CardContent>
+              <Box>
+              <Image onClick={()=>changeImgBg()} src={`/dashboard/persons/${imgBg}.png`} width={200} height={200} alt="" />
+              </Box>
+            </Stack>
           </Card>
         </Grid>
         <Grid item xs={4.5}>
@@ -32,22 +55,28 @@ const DashboardTopSection = () => {
 };
 
 const ClockComponant = () => {
-  const randomBg = Math.floor(Math.random() * 11) + 1;
+  const randomBg = () => Math.floor(Math.random() * 11) + 1;
+  const [imgBg, setImgBg] = useState<number>(randomBg);
 
-  const imgBg = randomBg;
-
-  const [time, setTime] = useState<string>(new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
+  const [time, setTime] = useState<string>(
+    new Date().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })
+  );
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setTime(new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
+      setTime(
+        new Date().toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        })
+      );
     }, 1000);
 
-    return () => clearInterval(interval); // Cleanup on unmount
+    return () => clearInterval(interval);
   }, []);
 
   return (
-    <Box>
+    <Box onClick={() => setImgBg(randomBg)}>
       <Card
         sx={{
           backgroundImage: `url(/dashboard/clock_bg/${imgBg}.jpg)`,
@@ -64,8 +93,8 @@ const ClockComponant = () => {
             justifyContent={"center"}
           >
             <Typography
-              variant="h4"
               sx={{
+                userSelect: "none",
                 fontSize: "6.5rem",
                 color: "#fff",
                 textShadow: "1px 4px 10px #070421a3",
