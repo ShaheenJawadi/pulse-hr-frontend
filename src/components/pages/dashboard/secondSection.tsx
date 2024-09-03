84b08ad9c1f3a89e90c412784f262fa422bbd77b
shaheenj <jawadishaheen@gmail.com>
2024-09-03 17:52:56 +0100

    refactor: Update secondSection component layout, add  AttendanceByDeP components

--- src/components/pages/dashboard/secondSection.tsx
+++ src/components/pages/dashboard/secondSection.tsx
@@ -13,7 +13,7 @@ import {
   useTheme,
 } from "@mui/material";
 import Image from "next/image";
-import LinearProgress from '@mui/material/LinearProgress';
+import LinearProgress from "@mui/material/LinearProgress";
 import { ApexOptions } from "apexcharts";
 import AppReactApexCharts from "@/components/charts/ApexChartWrapper";
 
@@ -23,9 +23,16 @@ const DashboardSecondSection = () => {
       <Grid item xs={3}>
         <EmpOfWeek />
       </Grid>
-      <Grid item xs={3}>
-        <TasksChart/>
+      <Grid item xs={9}>
+        
+         
+              <AttendanceByDeP />
+       
+        
       </Grid>
+      <Grid item xs={12}>
+              <TasksChart />
+            </Grid>
     </Grid>
   );
 };
@@ -47,174 +54,267 @@ const EmpOfWeek = () => {
         alignItems={"self-end"}
         justifyContent={"space-between"}
       >
-        <CardContent sx={{ height: "100%" ,width:"100%"}}>
-          
-            <Stack spacing={2} >
-              <Typography variant="h5" color={"white"}>
-                Employé de la semaine{" "}
-                <Typography component={"strong"} fontSize={45}>
-                  {" "}
-                  🎊
-                </Typography>
+        <CardContent sx={{ height: "100%", width: "100%" }}>
+          <Stack spacing={2}>
+            <Typography variant="h5" color={"white"}>
+              Employé de la semaine{" "}
+              <Typography component={"strong"} fontSize={45}>
+                {" "}
+                🎊
               </Typography>
-              <Stack alignItems={"center"} position={"relative"}>
-                <Avatar
-                  sx={{ width: 250, height: 250 }}
-                  variant="rounded"
-                  src={"/utils/goat.jpg"}
-                />
-                <Box sx={{position:"absolute" , bottom:-50, right:0}}>
-
-               
+            </Typography>
+            <Stack alignItems={"center"} position={"relative"}>
+              <Avatar
+                sx={{ width: 250, height: 250 }}
+                variant="rounded"
+                src={"/utils/goat.jpg"}
+              />
+              <Box sx={{ position: "absolute", bottom: -50, right: 0 }}>
                 <Image
                   src={`/dashboard/items/emp-week-badge.png`}
                   width={120}
                   height={120}
                   alt=""
-                /> </Box>
-              </Stack>
-              <Typography color={"white"} textAlign={"center"} variant="h6">
-                Shaheen jawadi
-              </Typography>
+                />{" "}
+              </Box>
+            </Stack>
+            <Typography color={"white"} textAlign={"center"} variant="h6">
+              Shaheen jawadi
+            </Typography>
 
-              <Card>
-                <Stack  spacing={2}   padding={2} divider={<Divider/>}>
-                    
+            <Card>
+              <Stack spacing={2} padding={2} divider={<Divider />}>
                 <Stack
                   direction={"row"}
                   alignItems={"center"}
                   justifyContent={"space-between"}
-              
                   spacing={3}
                 >
-                     
                   <Stack spacing={2} flex={1}>
                     <Typography
                       variant="body2"
                       color={"secondary"}
                       fontWeight={600}
                     >
-                     Tâches accomplies
+                      Tâches accomplies
                     </Typography>
                     <Box>
-                                <LinearProgress variant="determinate" value={50} />  
+                      <LinearProgress variant="determinate" value={50} />
                     </Box>
-          
                   </Stack>
-              
-                    
-                  <Typography variant="h6" color={"success"}>95%</Typography>
-                
+
+                  <Typography variant="h6" color={"success"}>
+                    95%
+                  </Typography>
                 </Stack>
                 <Stack
                   direction={"row"}
                   alignItems={"center"}
                   justifyContent={"space-between"}
-              
                   spacing={3}
                 >
-                      
-                    <Typography
-                      variant="body2"
-                      color={"secondary"}
-                      fontWeight={600}
-                    >
+                  <Typography
+                    variant="body2"
+                    color={"secondary"}
+                    fontWeight={600}
+                  >
                     Score
-                    </Typography>
-                     
-                
-              
-                    
-                  <Typography variant="h6" color={"success"}>253</Typography>
-                
+                  </Typography>
+
+                  <Typography variant="h6" color={"success"}>
+                    253
+                  </Typography>
                 </Stack>
-                </Stack>
-              </Card>
-            </Stack>
-    
+              </Stack>
+            </Card>
+          </Stack>
         </CardContent>
       </Stack>
     </Card>
   );
 };
 
-
 const TasksChart = () => {
+  const theme = useTheme();
 
-    const theme = useTheme()
-
-     
- 
-    const options: ApexOptions = {
-      stroke: { dashArray: 8 },
-      labels: ['Tâche terminée'],
-      colors: ['var(--mui-palette-primary-main)'],
-      states: {
-        hover: {
-          filter: { type: 'none' }
+  const options: ApexOptions = {
+    stroke: { dashArray: 8 },
+    labels: ["Tâche terminée"],
+    colors: ["var(--mui-palette-primary-main)"],
+    states: {
+      hover: {
+        filter: { type: "none" },
+      },
+      active: {
+        filter: { type: "none" },
+      },
+    },
+    fill: {
+      type: "gradient",
+      gradient: {
+        shade: "dark",
+        opacityTo: 0.5,
+        opacityFrom: 1,
+        shadeIntensity: 0.5,
+        stops: [30, 70, 100],
+        inverseColors: false,
+        gradientToColors: ["var(--mui-palette-primary-main)"],
+      },
+    },
+    plotOptions: {
+      radialBar: {
+        endAngle: 130,
+        startAngle: -140,
+        hollow: { size: "60%" },
+        track: { background: "transparent" },
+        dataLabels: {
+          name: {
+            offsetY: -24,
+            color: "var(--mui-palette-text-disabled)",
+            fontFamily: theme.typography.fontFamily,
+            fontSize: theme.typography.body2.fontSize as string,
+          },
+          value: {
+            offsetY: 8,
+            fontWeight: 500,
+            formatter: (value) => `${value}%`,
+            color: "var(--mui-palette-text-primary)",
+            fontFamily: theme.typography.fontFamily,
+            fontSize: theme.typography.h3.fontSize as string,
+          },
         },
-        active: {
-          filter: { type: 'none' }
-        }
       },
-      fill: {
-        type: 'gradient',
-        gradient: {
-          shade: 'dark',
-          opacityTo: 0.5,
-          opacityFrom: 1,
-          shadeIntensity: 0.5,
-          stops: [30, 70, 100],
-          inverseColors: false,
-          gradientToColors: ['var(--mui-palette-primary-main)']
-        }
+    },
+    grid: {
+      padding: {
+        top: -18,
+        left: 0,
+        right: 0,
+        bottom: 14,
       },
-      plotOptions: {
-        radialBar: {
-          endAngle: 130,
-          startAngle: -140,
-          hollow: { size: '60%' },
-          track: { background: 'transparent' },
-          dataLabels: {
-            name: {
-              offsetY: -24,
-              color: 'var(--mui-palette-text-disabled)',
-              fontFamily: theme.typography.fontFamily,
-              fontSize: theme.typography.body2.fontSize as string
-            },
-            value: {
-              offsetY: 8,
-              fontWeight: 500,
-              formatter: value => `${value}%`,
-              color: 'var(--mui-palette-text-primary)',
-              fontFamily: theme.typography.fontFamily,
-              fontSize: theme.typography.h3.fontSize as string
-            }
+    },
+  };
+
+  return (
+    <Card>
+      <CardHeader title="Suivi des tickets" subheader="Les 7 derniers jours" />
+      <CardContent>
+        <AppReactApexCharts
+          type="radialBar"
+          height={300}
+          width="100%"
+          series={[35]}
+          options={options}
+        />
+      </CardContent>
+    </Card>
+  );
+};
+
+const AttendanceByDeP = () => {
+
+    const series =[
+        {
+          name: 'Present - Yesterday',
+          group: 'Yesterday',
+          data: [10, 12, 8, 15, 6, 9]   
+        },
+        {
+          name: 'Absent - Yesterday',
+          group: 'Yesterday',
+          data: [4, 6, 5, 3, 2, 4]     
+        },
+        {
+          name: 'Late - Yesterday',
+          group: 'Yesterday',
+          data: [2, 4, 3, 2, 1, 3]    
+        },
+        {
+          name: 'Present - Today',
+          group: 'Today',
+          data: [12, 15, 10, 18, 7, 10]   
+        },
+        {
+          name: 'Absent - Today',
+          group: 'Today',
+          data: [5, 7, 6, 4, 3, 5]     
+        },
+        {
+          name: 'Late - Today',
+          group: 'Today',
+          data: [3, 5, 4, 2, 2, 4]    
+        }
+      ];
+      
+      const options:ApexOptions={
+        chart: {
+          type: 'bar', 
+          stacked: true,
+        },
+        stroke: {
+          width: 1,
+          colors: ['#fff']
+        },
+        plotOptions: {
+          bar: {
+            horizontal: false,
+            columnWidth: '60%'  // Adjust column width for spacing
+          }
+        },
+        xaxis: {
+          categories: [
+            'Sales',
+            'Marketing',
+            'HR',
+            'Development',
+            'Support',
+            'Finance'
+          ]
+        },
+        fill: {
+          opacity: 1
+        },
+        colors: ['var(--mui-palette-primary-mainOpacity)', 'var(--mui-palette-error-mainOpacity)', 'var(--mui-palette-warning-mainOpacity)','var(--mui-palette-primary-main)', 'var(--mui-palette-error-main)', 'var(--mui-palette-warning-main)'],
+        yaxis: {
+          max: 45,
+          labels: {
+            formatter: (val) => val
+          }
+        },
+        legend: {
+          position: 'top',
+          horizontalAlign: 'left'
+        },
+        tooltip: {
+          shared: true,
+          intersect: false
+        },
+        grid: {
+          padding: {
+            top: -16,
+            left: -18,
+            right: -17,
+            bottom: -11
           }
         }
-      },
-      grid: {
-        padding: {
-          top: -18,
-          left: 0,
-          right: 0,
-          bottom: 14
-        }
-      },
-     
-    }
-  
-    return (
-      <Card>
-        <CardHeader
-          title='Suivi des tickets'
-          subheader='Les 7 derniers jours' 
-        />
-        <CardContent >
-         
-          <AppReactApexCharts  type='radialBar' height={300} width='100%' series={[35]} options={options} />
-        </CardContent>
-      </Card>
-    )
+      };
+
+      return (
+        <Card>
+          <CardHeader title="Suivi des départements" subheader="Hier vs Aujourd'hui" />
+          <CardContent>
+            <AppReactApexCharts
+              type="bar"
+              height={400}
+              width="100%" 
+              options={options} 
+              series={series} 
+            />
+          </CardContent>
+        </Card>
+      );
+      
+
+
 }
 export default DashboardSecondSection;
