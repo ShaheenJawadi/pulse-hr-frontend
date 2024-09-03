3fccdc5d685469eb1beda6a92e83ac0715848e6d
shaheenj <jawadishaheen@gmail.com>
2024-09-03 16:04:29 +0100

    refactor: Update layoutWrapper component to fix nesting issue

--- src/components/layout/layoutWrapper.tsx
+++ src/components/layout/layoutWrapper.tsx
@@ -13,14 +13,14 @@ import TopBar from "./topBar";
 const LayoutWrapper = ({ children }: ChildrenType) => {
   return (
     <Box>
-      <TopBar />
-      <Container maxWidth="xl" sx={{ height: "100vh", overflow: "hidden" }}>
-        <Box sx={{ height: `calc(100vh - ${navigationHeaderHeight}px)` }}>
-          <PerfectScrollbar>
-            <Box sx={{ paddingY: 5}}>{children}</Box>
-          </PerfectScrollbar>
-        </Box>
-      </Container>
+      <TopBar />{" "}
+      <Box sx={{ height: `calc(100vh - ${navigationHeaderHeight}px)` }}>
+        <PerfectScrollbar>
+          <Container maxWidth="xl" sx={{ height: "100vh", overflow: "hidden" }}>
+            <Box sx={{ paddingY: 5 }}>{children}</Box>
+          </Container> 
+        </PerfectScrollbar>
+      </Box>
     </Box>
   );
 };
